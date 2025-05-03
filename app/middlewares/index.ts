e06f2dccea270469c9525716003e181dbf164ed9
Sofiane <ls_yekene@esi.dz>
2025-05-03 13:25:49 +0100

    add request logger middleware for enhanced request tracking

--- app/middlewares/index.ts
+++ app/middlewares/index.ts
@@ -1,13 +1,18 @@
 import { FastifyInstance } from 'fastify';
 import fastifyCors from '@fastify/cors';
 import errorHandler from './errorHandlerMiddleware';
+import requestLoggerMiddleware from './RequestLoggerMiddleware';
 
 const registerMiddlewares = (fastify: FastifyInstance) => {
+
+  
+  requestLoggerMiddleware(fastify);
+  errorHandler(fastify);
   fastify.register(fastifyCors, {
     origin: '*',
   });
 
-  errorHandler(fastify);
+
 };
 
 export default registerMiddlewares;
